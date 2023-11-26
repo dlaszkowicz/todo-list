6a7a15319d0145a6f432fdf029b1286c5b599c5d
dlaszkowicz <d.laszkowicz@gmail.com>
2023-11-26 01:51:28 +0100

    can add tasks now

--- src/taskConstructor.js
+++ src/taskConstructor.js
@@ -1,6 +1,7 @@
 function taskCreator() {
+  const submitBtn = document.querySelector("#submit-btn");
+  const newTaskForm = document.querySelector(".task-form");
   const task = function taskFun(title, description, date) {
-    const newTaskForm = document.querySelector(".task-form");
     this.title = title;
     this.description = description;
     this.date = date;
@@ -10,17 +11,31 @@ function taskCreator() {
     const hero = document.querySelector(".hero");
     newTask.title = newTaskForm.title.value;
     newTask.description = newTaskForm.description.value;
-    newTask.date = newTaskForm.description.value;
+    newTask.date = newTaskForm.date.value;
 
     const task = document.createElement("div");
     task.setAttribute("class", "task");
 
     const checkmark = document.createElement("button");
+    task.appendChild(checkmark);
     hero.appendChild(task);
 
     const title = document.createElement("p");
     title.textContent = newTask.title;
     task.appendChild(title);
+
+    const description = document.createElement("p");
+    description.textContent = newTask.description;
+    task.appendChild(description);
+
+    const date = document.createElement("p");
+
+    task.appendChild(date);
+
+    hero.appendChild(newTask);
   }
+  submitBtn.addEventListener("click", function () {
+    addTask();
+  });
 }
 export default taskCreator;
